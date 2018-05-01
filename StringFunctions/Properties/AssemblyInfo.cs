7c13b2eea18ee5d976aa1ea32000e705ae6071f7
Shaun Wilkinson <shaun.wilkinson@sitekit.net>
2018-05-01 15:02:27 +0100

    Added CompareStrings function and mad eother elements more generic

--- StringFunctions/Properties/AssemblyInfo.cs
+++ StringFunctions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CompareOwnerToString")]
+[assembly: AssemblyTitle("String Functions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CompareOwnerToString")]
+[assembly: AssemblyProduct("StringFunctions")]
 [assembly: AssemblyCopyright("Copyright © SPandCRM.com 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
