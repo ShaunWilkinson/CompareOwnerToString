5b6201fb5229d3faec9a09a3d404b787631ca705
Shaun Wilkinson <shaun.wilkinson@sitekit.net>
2018-05-01 13:15:56 +0100

    Initial Commit

---
+++ CompareOwnerToString/CompareOwnerToString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace SPandCRM
+{
+    public class CompareOwnerToString : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Owner")]
+        public InArgument<String> Owner { get; set; }
+
+        [RequiredArgument]
+        [Input("String to Compare")]
+        public InArgument<String> ComparisonString { get; set; }
+
+        [Output("Is SYSTEM")]
+        public OutArgument<Boolean> EqualsString { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            // Get the input paramaters
+            String owner = Owner.Get<String>(executionContext);
+            String comparitor = ComparisonString.Get<String>(executionContext);
+
+            tracingService.Trace("Owner: " + owner);
+            
+            // Compare the inputted owner to comparison string then return true or false
+            if(owner.Equals(comparitor))
+            {
+                EqualsString.Set(executionContext, true);
+            } else
+            {
+                EqualsString.Set(executionContext, false);
+            }
+            
+        }
+    }
+}
--- CompareOwnerToString/Properties/AssemblyInfo.cs
+++ CompareOwnerToString/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("assignedToSYSTEM")]
+[assembly: AssemblyTitle("CompareOwnerToString")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("assignedToSYSTEM")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("CompareOwnerToString")]
+[assembly: AssemblyCopyright("Copyright © SPandCRM.com 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
